c548d56534ead75db98bff19d9054651bb690ad6
muberracatak <muberra.catak45@gmail.com>
2022-04-22 10:43:23 +0300

    message

--- src/test_geocode.js
+++ src/test_geocode.js
@@ -4,11 +4,10 @@ const test_geocode = (address, callback) => {
     const url_adres = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiY2FueWFoeWEiLCJhIjoiY2tucTZ2a3ZlMDI4ZzMzbnQzY29wd24wdiJ9.Stz8gEheY66xXHzzQTVZow&limit=1'
 
     request({url: url_adres,json: true },(error,response)=>{
-        if(error){
+        if(error)
             callback(error,undefined);
-        }else{
-            callback(undefined,response)
-        }
+        else
+            callback(undefined,response.body)
     })  
 }
 console.log(encodeURIComponent("weather appp"));   
